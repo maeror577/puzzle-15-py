f82226447b5199e9759e2e7c5df5ac59db206cb4
rippedpleasure <luvyou2day@gmail.com>
2020-11-17 20:36:27 +0500

    Add make_move method. Playable version

--- puzzle_15.py
+++ puzzle_15.py
@@ -27,6 +27,13 @@ class Board:
     def draw(self):
         """ Draw current state of the game board. """
         os.system(CLEAR_SCREEN)
+        screen_width = self.size * 8
+        print('-' * screen_width)
+        print('Game of Fifteen'.center(screen_width))
+        print()
+        print('controls: w a s d'.center(screen_width))
+        print('-' * screen_width)
+
         for row in self.current_state:
             print('┌──────┐' * self.size)
             print('|      |' * self.size)
@@ -39,11 +46,54 @@ class Board:
             print('|      |' * self.size)
             print('└──────┘' * self.size)
 
+        print('-' * screen_width)
+        print()
+
+    def find_empty(self):
+        """ Find coordinates of an empty cell. """
+        for empty_x, row in enumerate(self.current_state):
+            for empty_y, number in enumerate(row):
+                if number == self.end_state[-1][-1]:
+                    return empty_x, empty_y
+        return None
+
+    def make_move(self, direction):
+        """ Move tile to an empty place. """
+        if direction == 'w':
+            shift_x, shift_y = 1, 0
+        elif direction == 's':
+            shift_x, shift_y = -1, 0
+        elif direction == 'a':
+            shift_x, shift_y = 0, 1
+        elif direction == 'd':
+            shift_x, shift_y = 0, -1
+        empty_x, empty_y = self.find_empty()
+        (self.current_state[empty_x][empty_y],
+        self.current_state[empty_x + shift_x][empty_y + shift_y]) = \
+        (self.current_state[empty_x + shift_x][empty_y + shift_y],
+        self.current_state[empty_x][empty_y])
+
+    def is_win(self):
+        """ Check for a victory condition. """
+        return self.current_state == self.end_state
+
 
 def main():
     """ Main game function. """
     board = Board()
+    while not board.is_win():
+        board.draw()
+        try:
+            direction = input('Enter direction: ')
+            board.make_move(direction)
+        except (IndexError, ValueError, UnboundLocalError):
+            pass
     board.draw()
+    print('Puzzle solved!')
+
+    restart = input('Do you want to play again? (y/n) ')
+    if restart in ('y', 'Y'):
+        main()
 
 if __name__ == '__main__':
     main()
